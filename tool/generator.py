62a5e8d84fba0cc885cb775df851eeaea5ce04e1
Adem kouki <ademkingnew@gmail.com>
2019-07-28 11:48:48 +0100

    :rocket: Update generator

--- tool/generator.py
+++ tool/generator.py
@@ -1,8 +1,8 @@
-# Generate Icons Class
-
 import os
-directory = '../assets'
 
+assetDirectory = '../assets'
+
+# Generate Icons Class
 f = open("../lib/kouki_data.dart", "w")
 
 f.write("".join("class Koukicon{") + "\n")
@@ -11,7 +11,7 @@ f.write("".join("	Koukicon(this.icon);") + "\n")
 f.write("".join("	final String icon;") + "\n")
 
 
-for filename in os.listdir(directory):
+for filename in os.listdir(assetDirectory):
         var_name = filename.replace('.svg', '')
         line = "	static final Koukicon " + var_name, " = Koukicon('assets/" + str(filename) + "');"
         f.write("".join(line) + "\n")
@@ -19,3 +19,18 @@ for filename in os.listdir(directory):
 
 f.write("".join("}"))
 f.close()
+
+# Generate Icon List Markdow
+
+f1 = open("../ListIcons.md", "w")
+
+f1.write("".join("# List of Icons") + "\n\n")
+f1.write("".join("| Icon name  | Preview  | Code |") + "\n")
+f1.write("".join("|---|---|---|") + "\n")
+
+for filename in os.listdir(assetDirectory):
+        var_name = filename.replace('.svg', '')
+        f1.write("".join("| " + var_name + " | " + '<p align="center"><img width="50" src="assets/' + str(filename) + '"></p> | ' +  "Koukicons(Koukicon." + var_name +  ") |" ) + "\n")
+        continue
+
+f1.close()
